759ebc14750ee3b5f00f277e68be13aef702d453
Derek Dang <dqdang@users.noreply.github.com>
2020-05-20 18:07:16 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import requests 
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+LINK = os.getenv("LINK")
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='joy')
+async def nine_nine(ctx):
+    response = requests.get(LINK)
+    if response.ok:
+        response = response.json()
+        await ctx.send(response['message'])
+
+bot.run(TOKEN)
