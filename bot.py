4b9b9f2aa9bab2568fb811cee0c7808b5620db66
Derek Dang <dqdang17@gmail.com>
2020-05-20 18:52:19 -0700

    Testing scheduled messages

--- bot.py
+++ bot.py
@@ -1,19 +1,38 @@
+import discord.utils
 from discord.ext import commands
-from dotenv import load_dotenv
 import os
 import requests 
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL=os.getenv('')
 LINK = os.getenv("LINK")
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='joy')
-async def joy(ctx):
+async def send_joy():
     response = requests.get(LINK)
     if response.ok:
         response = response.json()
-        await ctx.send(response['message'])
+        return response['message']
+    return None
 
+async def check_time():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        existing_channel = discord.utils.get(guild.channels, name=channel_name)
+        now=datetime.strftime(datetime.now(),'%H:%M')
+        if now == send_time:
+            await bot.send_message(send_joy)
+        # await asyncio.sleep(60)
+
+@bot.event
+async def on_ready():
+    print(bot.user.name)
+    print(bot.user.id)
+
+@bot.command(name='joy')
+async def joy(ctx):
+    await ctx.send(send_joy())
+
+bot.loop.create_task(check_time())
 bot.run(TOKEN)
